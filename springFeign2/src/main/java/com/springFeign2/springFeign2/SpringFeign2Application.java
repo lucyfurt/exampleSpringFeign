f9c27b25526ab08a6f15759bac246bb52a5ce4df
lucyfurt <lucianafurtado10@gmail.com>
2022-05-20 23:57:07 -0300

    projeto com springOpenFeign

---
+++ springFeign2/src/main/java/com/springFeign2/springFeign2/SpringFeign2Application.java
@@ -0,0 +1,13 @@
+package com.springFeign2.springFeign2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFeign2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFeign2Application.class, args);
+	}
+
+}
---
+++ springFeign2/src/test/java/com/springFeign2/springFeign2/SpringFeign2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springFeign2.springFeign2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFeign2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
