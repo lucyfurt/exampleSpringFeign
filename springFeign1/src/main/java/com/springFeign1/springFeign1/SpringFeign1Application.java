4f2383c53774af8607f212ff80bb4584e4f03e60
lucyfurt <lucianafurtado10@gmail.com>
2022-05-20 23:22:36 -0300

    conceito sobre springFeign

---
+++ springFeign1/src/main/java/com/springFeign1/springFeign1/SpringFeign1Application.java
@@ -0,0 +1,13 @@
+package com.springFeign1.springFeign1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFeign1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFeign1Application.class, args);
+	}
+
+}
---
+++ springFeign1/src/test/java/com/springFeign1/springFeign1/SpringFeign1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springFeign1.springFeign1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFeign1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
